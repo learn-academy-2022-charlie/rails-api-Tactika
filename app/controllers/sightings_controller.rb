9fe1e1207b09137359eed805e26025cc3c835f06
Damien Richcreek <damien@richcreek.dev>
2022-06-17 14:13:46 -0700

    created sighting update api endpoint and the show endpoint

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,4 +1,5 @@
 class SightingsController < ApplicationController
+  # Index API Method - Show all Sightings
   def index
     sightings = Sighting.all
 
@@ -7,5 +8,29 @@ class SightingsController < ApplicationController
     else
       render json:sightings.errors
     end
+  end
+
+  def show
+    sighting = Sighting.find(params[:id])
+
+    if sighting.valid?
+      render json:sighting
+    else
+      render json:sighting.errors
+    end
+  end
+  # Update API Method
+  def update
+    sighting = Sighting.find(params[:id])
+    if sighting.update(sighting_params)
+      render json:sighting
+    else
+      render json:sighting.errors
+    end
+  end
+  # Validation Method
+  def sighting_params
+    params.require(:sighting).permit(:date, :latitude, :longitude, :animal_id)
+
   end
 end
