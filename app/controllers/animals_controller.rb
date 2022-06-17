b55ce3292e94d2f23874e49e778e44d8b8470214
Damien Richcreek <damien@richcreek.dev>
2022-06-17 09:38:22 -0700

    created animal controller index api endpoint

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,10 @@
 class AnimalsController < ApplicationController
+  def index
+    animals = Animal.all
+    if animals
+      render json:animals
+    else
+      render json:animals.errors
+    end
+  end
 end
