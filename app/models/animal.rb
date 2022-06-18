378578b5d68fa4805e4895f49efef5850ab460dd
Damien Richcreek <damien@richcreek.dev>
2022-06-17 20:55:47 -0700

    created testing for animal to require common name and latin name inputs

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,5 @@
 class Animal < ApplicationRecord
   has_many :sightings
+
+  validates :common_name, :latin_name, presence: true
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Animal should require a common name input' do
+    common_name_input = Animal.create common_name: nil, latin_name: "Big Baddie Wolf", kingdom:"Animalia"
+    expect(common_name_input.errors[:common_name]).to_not be_empty
+  end
+
+  it 'Animal should require a latin name input' do
+    common_name_input = Animal.create common_name: "Grey Wolf", latin_name: nil, kingdom:"Animalia"
+    expect(common_name_input.errors[:latin_name]).to_not be_empty
+  end
+
 end
