7627798f1a2f02b01540ff625a1f1c89152e20d4
Damien Richcreek <damien@richcreek.dev>
2022-06-17 21:17:40 -0700

    hotfix - rearranged testing suite lines

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -7,19 +7,24 @@ RSpec.describe Animal, type: :model do
   end
 
   it 'Animal should require a latin name input' do
-    common_name_input = Animal.create common_name: "Grey Wolf", latin_name: nil, kingdom:"Animalia"
-    expect(common_name_input.errors[:latin_name]).to_not be_empty
+    latin_name_input = Animal.create common_name: "Grey Wolf", latin_name: nil, kingdom:"Animalia"
+    expect(latin_name_input.errors[:latin_name]).to_not be_empty
+  end
+
+  it 'Animal should require common name and latin name to not equal each other' do
+    equal_names_input = Animal.create common_name: "Wolf Grey", latin_name: "Canis Lupus", kingdom:"Animalia"
+    expect(equal_names_input.common_name.downcase).not_to equal(equal_names_input.latin_name.downcase)
   end
 
   it 'Animal should require a common name to be unique' do
     Animal.create common_name: "Grey Wolf", latin_name: "Canis Lupus", kingdom:"Animalia"
-    common_name_input = Animal.create common_name: "Grey Wolf", latin_name: "Lupus Canis", kingdom:"Animalia"
-    expect(common_name_input.errors[:common_name]).to_not be_empty
+    unique_common_name_input = Animal.create common_name: "Grey Wolf", latin_name: "Lupus Canis", kingdom:"Animalia"
+    expect(unique_common_name_input.errors[:common_name]).to_not be_empty
   end
 
   it 'Animal should require a latin name to be unique' do
     Animal.create common_name: "Grey Wolf", latin_name: "Canis Lupus", kingdom:"Animalia"
-    common_name_input = Animal.create common_name: "Wolf Grey", latin_name: "Canis Lupus", kingdom:"Animalia"
-    expect(common_name_input.errors[:latin_name]).to_not be_empty
+    unique_latin_name_input = Animal.create common_name: "Wolf Grey", latin_name: "Canis Lupus", kingdom:"Animalia"
+    expect(unique_latin_name_input.errors[:latin_name]).to_not be_empty
   end
 end
